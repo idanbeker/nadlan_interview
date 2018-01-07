ab58b4e09a65f3143d762c4d1025c0d4ff0ea57f
Idan Beker <idan@kilometer.io>
2018-01-07 11:58:51 +0200

    Finished design for weather view page

--- src/routes/CityWeatherScreen/components/CityWeatherScreen.react.js
+++ src/routes/CityWeatherScreen/components/CityWeatherScreen.react.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import {OPEN_WEATHER_API_KEY} from "../../../constants/OpenWeatherApi";
 import {GET_WEATHER_URL} from "../../../constants/Urls";
+import Spinner from 'react-spinkit';
 
 class CityWeatherScreen extends Component {
 
@@ -23,8 +24,8 @@ class CityWeatherScreen extends Component {
 
 
         try{
-            let {data:{weather:[weatherDescription],main:weatherData}} = await axios.get(GET_WEATHER_URL + `?appid=${OPEN_WEATHER_API_KEY}&id=${locationId}`)
-            this.setState({weatherDescription,weatherData,status:CityWeatherScreen.STATUS_SUCCESS})
+            let {data:{weather:[weatherDescription],main:weatherData,name:cityName}} = await axios.get(GET_WEATHER_URL + `?appid=${OPEN_WEATHER_API_KEY}&id=${locationId}&units=metric`)
+            this.setState({weatherDescription,weatherData,status:CityWeatherScreen.STATUS_SUCCESS,cityName})
         }catch(Error){
             console.error(Error)
             alert('Error fetching data')
@@ -35,14 +36,25 @@ class CityWeatherScreen extends Component {
 
 
     render() {
-        let {weatherDescription,weatherData} = this.state;
+        let {weatherDescription,weatherData,cityName,status} = this.state;
 
         console.log(weatherData,'WEATHER DATA', weatherDescription, 'WEATHER DESCRIPTIO');
 
 
         return (
-            <div className="CityWeatherScreen">
-                city weather screen
+            <div id="WeatherDisplay">
+                {status === CityWeatherScreen.STATUS_LOADING && <Spinner name="three-bounce" />}
+                {status === CityWeatherScreen.STATUS_SUCCESS &&
+                <div className="WeatherDataContainer">
+                    <div className="Title">Weather report for {cityName} </div>
+                    <img className="Icon" src={`http://openweathermap.org/img/w/${weatherDescription.icon}.png` }/>
+                    <div className="Degrees">{weatherData.temp} &#x2103;</div>
+                    <hr/>
+                    <div className="Description">Description: {weatherDescription.description}</div>
+                    <div className="Description">Pressure: {weatherData.pressure}</div>
+                    <div className="Description">Humidity: {weatherData.humidity}</div>
+                </div>
+                }
             </div>
         );
     }
